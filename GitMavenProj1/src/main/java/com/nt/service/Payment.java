020ba730ada7c21d7bb813aa1a99e31a2fc4a521
mutha <mutha@192.168.1.34>
2024-07-14 22:39:35 +0530

    Dev1--BharatPe

--- GitMavenProj1/src/main/java/com/nt/service/Payment.java
+++ GitMavenProj1/src/main/java/com/nt/service/Payment.java
@@ -7,7 +7,13 @@ public class Payment {
 	public String doUpiPayment() {
 		return "Upi Payment is done";
 	}
+
 	public String payPall(String url) {
 		return "Paypall integration is done";
+	}
+	public String enableBharatPe() {
+		//BharatPe configuration is going on
+		return "bharat pe integration is done";
+
 	}
 }
