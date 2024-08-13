9fd9f140c46e93b9e28cf86f66edfe57118bb959
mutha <mutha@host.docker.internal>
2024-08-13 14:41:23 +0530

    DockerSBApp1

---
+++ DockerSBApp1/src/main/java/com/nt/DockerSbApp1Application.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSbApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSbApp1Application.class, args);
+		System.out.println("welcome to Doccer Apps");
+	}
+
+}
